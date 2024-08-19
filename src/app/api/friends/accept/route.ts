785959b79185c0c6f24ffb66df191e04760084bc
Minh Hung <nlmhung2511@gmail.com>
2024-08-19 18:39:32 +0700

    add more api routes (accept and deny reqs)

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,18 +1,51 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/src/lib/auth";
 import { fetchRedis } from "@/src/commands/redis";
-
-
+import { z } from "zod";
+import { db } from "@/src/lib/db";
 export async function POST(req: Request) {
-    try{
+  try {
     const session = await getServerSession(authOptions);
     if (!session) {
       return new Response("You are unauthorized!", { status: 402 });
     }
 
-    return Response.json("OK", { status: 200 });
+    const body = await req.json();
+    const { id: idToAdd } = z.object({ id: z.string() }).parse(body);
+
+    const isFriends = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd
+    );
+    if (isFriends) {
+      return new Response("Already friends", { status: 400 });
     }
-    catch (error) {
-        return new Response("Something went wrong!", { status: 400 });
-      }
+
+    const hasSentRequest = (await fetchRedis(
+      "hexists",
+      `user:${session.user.id}:incoming_friend_requests`,
+      idToAdd
+    )) as 0 | 1;
+
+    if (!hasSentRequest) {
+      return new Response("Request doesn't exist!", {
+        status: 400,
+      });
+    }
+
+    await Promise.all([
+      fetchRedis("sadd", `user:${session.user.id}:friends`, idToAdd),
+      fetchRedis("sadd", `user:${idToAdd}:friends`, session.user.id),
+      fetchRedis(
+        "hdel",
+        `user:${session.user.id}:incoming_friend_requests`,
+        idToAdd
+      ),
+    ]);
+
+    return Response.json("OK", { status: 200 });
+  } catch (error) {
+    return new Response("Something went wrong!", { status: 400 });
+  }
 }
--- src/app/api/friends/deny/route.ts
+++ src/app/api/friends/deny/route.ts
@@ -1,6 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/src/lib/auth";
 import { fetchRedis } from "@/src/commands/redis";
+import { z } from "zod";
 
 
 export async function POST(req: Request) {
@@ -10,6 +11,35 @@ export async function POST(req: Request) {
       return new Response("You are unauthorized!", { status: 402 });
     }
 
+    const body = await req.json();
+    const { id: idToAdd } = z.object({ id: z.string() }).parse(body);
+    
+    const isFriends = await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd
+    );
+    if (isFriends) {
+      return new Response("Already friends", { status: 400 });
+    }
+
+    const hasSentRequest = (await fetchRedis(
+      "hexists",
+      `user:${session.user.id}:incoming_friend_requests`,
+      idToAdd
+    )) as 0 | 1;
+
+    if (!hasSentRequest) {
+      return new Response("Request doesn't exist!", {
+        status: 400,
+      });
+    }
+
+    await fetchRedis(
+      "hdel",
+      `user:${session.user.id}:incoming_friend_requests`,
+      idToAdd
+    )
     return Response.json("OK", { status: 200 });
     }
     catch (error) {
