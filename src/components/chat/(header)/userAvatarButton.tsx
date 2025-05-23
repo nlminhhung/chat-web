e521b274c5ade34b8395dc3725ea8c96e4876083
Hung Nguyen <nlmhung2511@gmail.com>
2025-05-23 08:52:40 +0700

    chores: edit image sizes

--- src/components/chat/(header)/userAvatarButton.tsx
+++ src/components/chat/(header)/userAvatarButton.tsx
@@ -13,20 +13,22 @@ import { Button } from "@/src/components/chat/ui/button";
 import ToAdminButton from "./toAdminButton";
 import Image from "next/image";
 import ProfileCustomizer from "@/src/components/chat/(header)/profileCustomizer";
+import { Avatar, AvatarImage } from "@/src/components/chat/ui/avatar"
 
 export default async function UserAvatarButton() {
   const session = await getServerSession(authOptions);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="rounded-full">
-          <Image src={session!.user.image!}
+          {/* <Image src={session!.user.image!}
                 alt={session!.user.name}
                 width={40}
                 height={40}
                 className="rounded-full object-cover"/>
-          <span className="sr-only">Toggle user menu</span>
-        </Button>
+          <span className="sr-only">Toggle user menu</span> */}
+         <Avatar role="button" className="h-10 w-10 mr-3 hover:opacity-80 transition">
+              <AvatarImage src={session!.user.image!} />
+          </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
